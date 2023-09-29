b6501f6c66133227b75c201768dbad49b157fe34
Pedro Gardim <me@pedrogardim.com>
2023-09-29 16:37:26 +0200

    feat: add border and score

--- src/utils/display.js
+++ src/utils/display.js
@@ -1,26 +1,26 @@
 import gameState from "../core/game.js";
 
 export const draw = () => {
-  let { started, gameSize, applePosition, snakeTiles } = gameState;
+  let { started, gameSize, applePosition, snakeTiles, points } = gameState;
   console.clear();
   if (!started) {
     console.log("Press enter to start");
     return;
   }
 
-  for (let x = 0; x < gameSize[1] + 2; x++) {
-    let line = "";
+  console.log(`Score: ${points}`);
+  console.log("⌑" + Array(gameSize[0]).fill("⎯").join("") + "⌑");
+  for (let x = 0; x < gameSize[1]; x++) {
+    let line = "|";
     for (let y = 0; y < gameSize[0]; y++) {
       line += snakeTiles.find((a) => a[0] === y && a[1] === x)
         ? "█"
         : applePosition[0] === y && applePosition[1] === x
-        ? "O"
+        ? "⚛︎"
         : " ";
     }
+    line += "|";
     console.log(line);
   }
-
-  return;
-  ctx.textAlign = "left";
-  ctx.fillText(`Score: ${points}`, 4, canvas.height / 15);
+  console.log("⌑" + Array(gameSize[0]).fill("⎯").join("") + "⌑");
 };
